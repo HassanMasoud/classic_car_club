149c4bc9c76e783abeef50b284c7b1a61f150c51
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-21 19:13:42 +0300

    nav and form styling

--- routes/cars.js
+++ routes/cars.js
@@ -19,6 +19,42 @@ const cars = [
     model: "Range Rover",
     image:
       "https://images.pexels.com/photos/103290/pexels-photo-103290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    make: "Mitsubishi",
+    model: "Galant",
+    image:
+      "https://images.pexels.com/photos/1008659/pexels-photo-1008659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    make: "Toyota",
+    model: "Rav4",
+    image:
+      "https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    make: "Land Rover",
+    model: "Range Rover",
+    image:
+      "https://images.pexels.com/photos/103290/pexels-photo-103290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    make: "Mitsubishi",
+    model: "Galant",
+    image:
+      "https://images.pexels.com/photos/1008659/pexels-photo-1008659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    make: "Toyota",
+    model: "Rav4",
+    image:
+      "https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    make: "Land Rover",
+    model: "Range Rover",
+    image:
+      "https://images.pexels.com/photos/103290/pexels-photo-103290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
   }
 ];
 
