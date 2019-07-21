12a4d1a47cef494ce20923aa89a3f9f03b255201
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-21 17:58:28 +0300

    creating cars route

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
+app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const carsRoute = require("./routes/cars");
 
--- routes/cars.js
+++ routes/cars.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const router = express.Router();
 
+const cars = [
+  {
+    make: "Mitsubishi",
+    model: "Galant",
+    image:
+      "https://images.pexels.com/photos/1008659/pexels-photo-1008659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+  },
+  {
+    make: "Toyota",
+    model: "Rav4",
+    image:
+      "https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    make: "Land Rover",
+    model: "Range Rover",
+    image:
+      "https://images.pexels.com/photos/103290/pexels-photo-103290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
+];
+
 router.get("/", (req, res) => {
-  const cars = [
-    {
-      make: "Mitsubishi",
-      model: "Galant",
-      image:
-        "https://images.pexels.com/photos/1008659/pexels-photo-1008659.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-    },
-    {
-      make: "Toyota",
-      model: "Rav4",
-      image:
-        "https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    },
-    {
-      make: "Land Rover",
-      model: "Range Rover",
-      image:
-        "https://images.pexels.com/photos/103290/pexels-photo-103290.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-    }
-  ];
   res.render("cars", { cars: cars });
+});
+
+router.post("/", (req, res) => {
+  const make = req.body.make;
+  const model = req.body.model;
+  const image = req.body.image;
+  const newCar = { make: make, model: model, image: image };
+  cars.push(newCar);
+  res.redirect("/cars");
+});
+
+router.get("/new", (req, res) => {
+  res.render("new");
 });
 
 module.exports = router;
