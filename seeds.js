3ff2000e791c48d2f49ae71cfd3c302df7728d28
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-22 12:58:43 +0300

    car show page styling

--- seeds.js
+++ seeds.js
@@ -8,21 +8,24 @@ const data = [
     model: "Beetle",
     image:
       "https://images.pexels.com/photos/1209774/pexels-photo-1209774.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    description: "Red VW Beetle"
+    description:
+      'The Volkswagen Beetle — officially the Volkswagen Type 1, informally in German the Käfer (meaning "beetle"), in parts of the English-speaking world the Bug, and known by many other nicknames in other languages—is a two-door, rear-engine economy car, intended for five occupants (later, Beetles were restricted to four people in some countries), that was manufactured and marketed by German automaker Volkswagen (VW) from 1938 until 2003.'
   },
   {
     make: "Ford",
     model: "Galaxie 500",
     image:
       "https://images.pexels.com/photos/97458/pexels-photo-97458.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    description: "Black Ford Galaxie"
+    description:
+      'The Ford Galaxie is a full-sized car that was built in the United States of America by Ford for model years 1959 through to 1974. The name was used for the top models in Ford\'s full-size range from 1958 until 1961, in a marketing attempt to appeal to the excitement surrounding the Space Race. For 1962, all full-size Fords wore the Galaxie badge, with "500" and "500/XL" denoting the higher series. '
   },
   {
     make: "AC",
     model: "Cobra 427",
     image:
       "https://images.pexels.com/photos/149813/pexels-photo-149813.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-    description: "Blue and white AC Cobra"
+    description:
+      "The AC Cobra, sold as the Shelby Cobra in the United States, is an Anglo-American sports car with a Ford V8 engine, produced intermittently in both the UK and the US since 1962."
   }
 ];
 
