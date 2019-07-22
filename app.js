32e9eac497f5c7852e69a4fea020c9c6009c6353
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-22 14:35:04 +0300

    registering users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 const seedDB = require("./seeds");
 
 dotenv.config();
@@ -12,6 +13,20 @@ dotenv.config();
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }, () =>
   console.log("Connected to MongoDB")
 );
+
+app.use(
+  require("express-session")({
+    secret: "Once again Rusty wins cutest dog!",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -23,12 +38,14 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 seedDB();
 
 const carsRoute = require("./routes/cars");
+const authRoute = require("./routes/auth");
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.use("/cars", carsRoute);
+app.use("/register", authRoute);
 
 app.listen(process.env.PORT || 3000, process.env.IP, () =>
   console.log("Server started")
---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("../models/user");
+
+router.get("/", (req, res) => {
+  res.render("register");
+});
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  const newUser = new User({ username: username });
+  try {
+    const user = await User.register(newUser, password);
+    await passport.authenticate("local")(req, res, () => {
+      res.redirect("/cars");
+    });
+    console.log(user);
+  } catch (err) {
+    console.log(err);
+    return res.render("register");
+  }
+});
+
+module.exports = router;
